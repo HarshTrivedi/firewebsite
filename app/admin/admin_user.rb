6a58403e0868d1b0d2cb42e3999abd5c213c5868
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-12 23:01:54 +0530

    update admin model in active_admin (index action and filters)

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -4,18 +4,13 @@ ActiveAdmin.register AdminUser do
 
   index do
     selectable_column
-    id_column
     column :email
-    column :current_sign_in_at
     column :sign_in_count
     column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
 
   form do |f|
     f.inputs "Admin Details" do
