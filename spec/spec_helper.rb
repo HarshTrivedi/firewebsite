10df36dd568b3b51c723eed6e46eea226abde7e0
Prelang Builder <builder@prelang.com>
2015-05-12 05:26:24 +0000

    Added DatabaseCleaner to 'spec_helper.rb' with default strategy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,33 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  #->Prelang[testing_framework] -->
+  
+  config.before(:suite) do
+    DatabaseCleaner.start
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  
+  #<-- Prelang[testing_framework]
+
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
