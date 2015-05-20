bcc498da7ea3d7e030dbd994ac606c4e0c93a8cf
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-20 12:45:07 +0530

    add iframe support in rails app

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Firewebsite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
+
   end
 end
