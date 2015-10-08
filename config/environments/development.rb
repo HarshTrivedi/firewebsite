40937d513368692f3cf736ea9e425db9e14ac484
Shubham Patel <shubhpatel108@gmail.com>
2015-10-09 04:36:41 +0530

    Add SMTP settings for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,6 @@ Rails.application.configure do
   #->Prelang
   GA.tracker = ""
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
-
