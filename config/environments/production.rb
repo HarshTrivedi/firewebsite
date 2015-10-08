e54886810881635e454fd62253ce626ea87c2e04
Shubham Patel <shubhpatel108@gmail.com>
2015-10-09 04:49:17 +0530

    Update the domain in prod SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'fire2015.herokuapp.com',
     :enable_starttls_auto => true
   }
 
