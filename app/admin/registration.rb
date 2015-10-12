195126061aa4ff0287e323ebef3b7ab386b93a71
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-10-13 00:36:15 +0530

    customize admin page for registrations

--- app/admin/registration.rb
+++ app/admin/registration.rb
@@ -1,5 +1,31 @@
 ActiveAdmin.register Registration do
 	menu false
 	belongs_to :year , :optional => true
+  	permit_params :approved
+
+	index do
+	  selectable_column
+	  column :registration_number
+	  column :name
+	  column :affiliation
+	  column :nationality
+	  column :email
+	  column :phone
+	  # column "ACM member ?", :acm
+	  column "ACM membership number", :acmnum
+	  column :phone
+	  column :approved
+	  column :registration_type
+      column :created_at
+	  actions
+	end
+
+	form do |f|
+	  f.inputs "Approval Permission" do
+	    f.input :approved
+	  end
+	  f.actions
+	end
+
 
 end
