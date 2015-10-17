cc6ca6ae6b3b76f03f3214e313296fad53509543
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-10-18 02:12:37 +0530

    fix a typo

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -25,19 +25,19 @@ class Registration < ActiveRecord::Base
 
 		if self.acm # IS ACM Member
 			if self.occupation == 1 # Student
-				"3900 INR"
+				return "3900 INR"
 			elsif self.occupation == 2 # Industry
-				"5000 INR"
+				return "5000 INR"
 			elsif self.occupation == 3 # Academia
-				"5500 INR"
+				return "5500 INR"
 			end				
 		else # IS Not ACM Member
 			if self.occupation == 1 # Student
-				"4900 INR"
+				return "4900 INR"
 			elsif self.occupation == 2 # Industry
-				"6000 INR"
+				return "6000 INR"
 			elsif self.occupation == 3 # Academia
-				"12000 INR"
+				return "12000 INR"
 			end				
 		end
 
