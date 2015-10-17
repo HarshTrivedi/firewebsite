302332bf30b518785eb1e43e329a2bf9624d083c
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-10-18 02:11:35 +0530

    Add code for automatic calculation of amount to be paid

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -17,8 +17,31 @@ class Registration < ActiveRecord::Base
 
 
 	def amount
-		## calculation
-		return 0
+
+		## calculation 
+		if self.nationality != "IN"
+			return "250 USD"
+		end
+
+		if self.acm # IS ACM Member
+			if self.occupation == 1 # Student
+				"3900 INR"
+			elsif self.occupation == 2 # Industry
+				"5000 INR"
+			elsif self.occupation == 3 # Academia
+				"5500 INR"
+			end				
+		else # IS Not ACM Member
+			if self.occupation == 1 # Student
+				"4900 INR"
+			elsif self.occupation == 2 # Industry
+				"6000 INR"
+			elsif self.occupation == 3 # Academia
+				"12000 INR"
+			end				
+		end
+
+		return "Undefined"
 	end
 
 end
