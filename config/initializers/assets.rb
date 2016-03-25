5d5325072af623131512a8064d366d64a6b9b023
Harsh Trivedi <harshjtrivedi94@gmail.com>
2016-03-26 00:47:23 +0530

    fix problems in pushing code to heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+Rails.application.config.assets.precompile += ['materialize.css', "materialNote.css", "codeMirror/codemirror.css", "codeMirror/monokai.css", "main.css", "web.css"]
